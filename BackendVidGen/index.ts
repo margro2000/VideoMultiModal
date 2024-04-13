8312e6580602cd63a2ddd03aae8f1003d0e0a7e8
djacobs7 <daniel@sibblingz.com>
2024-04-13 13:19:38 -0700

    remove generator

--- BackendVidGen/index.ts
+++ BackendVidGen/index.ts
@@ -2,9 +2,27 @@ import { generateVerticalScroll, takeSnapshot } from "./screenshot";
 
 console.log("Hello via Bun!");
 
-let filename = await takeSnapshot("https://www.oracle.com/cloud/")
-
-generateVerticalScroll(filename)
 
 
 
+import { Elysia } from 'elysia'
+import { cors } from "@elysiajs/cors";
+
+const app = new Elysia();
+
+app.use(cors({
+
+    origin: "*"
+}));
+
+type GenerationParams = { url: string, prompt: string };
+app.post('/generate', async ({ params: GenerationParams }) => {
+
+    let filename = await takeSnapshot("https://www.oracle.com/cloud/")
+    generateVerticalScroll(filename)
+
+
+    return "DONE"
+
+});
+app.listen(8000);
